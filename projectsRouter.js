0ab8ff2a5fe7f33e464afb247b3cfee8fd3d4b40
Tyler Kummer <tylerkummer@protonmail.com>
2020-04-10 10:20:01 -0500

    GET

--- projectsRouter.js
+++ projectsRouter.js
@@ -3,4 +3,14 @@ const router = require("express").Router();
 const Actions = require("./data/helpers/actionModel");
 const Projects = require("./data/helpers/projectModel");
 
+router.get("/", (req, res) => {
+  Projects.get()
+    .then((project) => {
+      res.status(200).json(project);
+    })
+    .catch((error) => {
+      res.status(400).json({ message: "Database Error" });
+    });
+});
+
 module.exports = router;
