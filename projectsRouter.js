e8889a0a258c41bc1ea86d8117b5183545fb71d6
Tyler Kummer <tylerkummer@protonmail.com>
2020-04-10 10:31:34 -0500

    GET and Middleware

--- projectsRouter.js
+++ projectsRouter.js
@@ -9,8 +9,39 @@ router.get("/", (req, res) => {
       res.status(200).json(project);
     })
     .catch((error) => {
-      res.status(400).json({ message: "Database Error" });
+      console.log(error);
+      res.status(400).json({ error: "Database Error" });
     });
 });
+
+router.get("/:id", validateProjectId, (req, res) => {
+  res.status(200).json(req.project);
+});
+
+router.get("/:id/actions", validateProjectId, (req, res) => {
+  Projects.getProjectActions(req.params.id)
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "Database Error" });
+    });
+});
+
+function validateProjectId(req, res, next) {
+  Projects.get(req.params.id)
+    .then((project) => {
+      if (!project) {
+        res.status(400).json({ message: "invalid project id" });
+      } else {
+        req.project = project;
+        next();
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Database error" });
+    });
+}
 
 module.exports = router;
