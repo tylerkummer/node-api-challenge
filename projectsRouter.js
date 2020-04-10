bd3a3f010fc25e1bf5b4832bb0565ea7d54b3086
Tyler Kummer <tylerkummer@protonmail.com>
2020-04-10 11:25:42 -0500

    PUT

--- projectsRouter.js
+++ projectsRouter.js
@@ -80,7 +80,31 @@ router.delete("/:id/actions/:action_id", validateProjectId, (req, res) => {
     });
 });
 
-// middlware function
+// PUT project
+router.put("/:id", validateProjectId, (req, res) => {
+  Projects.update(req.params.id, req.body)
+    .then((project) => {
+      res.status(200).json(project);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "Database Error" });
+    });
+});
+
+// PUT action
+router.put("/:id/actions/:action_id", validateProjectId, (req, res) => {
+  Actions.update(req.params.action_id, req.body)
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "Database Error" });
+    });
+});
+
+// Middlware function
 function validateProjectId(req, res, next) {
   Projects.get(req.params.id)
     .then((project) => {
